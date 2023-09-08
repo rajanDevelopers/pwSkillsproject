ec08b64c6a542f8c3d93c08e26e4cf15a30eaedc
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2023-09-08 01:51:25 +0000

    Function About

---
+++ function.js
@@ -0,0 +1,66 @@
+// console.log("hii there ");
+
+// function parameter and Argument 
+
+// function greet() {
+//     console.log("Hii there ");
+//     console.log("Hello pw Skills Learner ");
+// }
+// greet();
+
+
+// function sqx(x) {
+
+//     console.log(x * x);
+// }
+// sqx(5);
+
+// function main(a) {
+//     return a + a;
+
+// }
+// let result = main(10); //  a => Argument
+
+// console.log(result);
+
+// Logic Build Base on Function 
+
+// function user(a){
+//     if(a == 10){
+//         console.log("user is Valided");
+//     }
+//     else{
+//         console.log("User Unkonw");
+//     }
+// }
+// user(100);
+
+// // Addintion Fuction 
+
+// function AdditiOn(m,n){
+//     return m * n;
+// }
+// let result1 = AdditiOn(10,5);
+// console.log(result1);
+
+
+function syMessage() {
+    console.log("Hii Pw Skills Learner & How are You Brother");
+
+}
+// syMessage();
+// syMessage();
+// syMessage(); 
+// syMessage();
+// syMessage(); 
+
+function passMessage() {
+    return "Gnanamani College of Technology Student ThanK You For Your Time "
+
+} 
+// Calling Function 
+ let Message = passMessage();
+ console.log(Message);
+
+
+
