cc719f5765ee3b27f315e2e21451698e1c8b260f
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2023-09-08 01:53:31 +0000

    function Details

--- function.js
+++ function.js
@@ -1,58 +1,59 @@
-// console.log("hii there ");
+// Testing Purpose 
+//console.log("hii there ");
 
 // function parameter and Argument 
 
-// function greet() {
-//     console.log("Hii there ");
-//     console.log("Hello pw Skills Learner ");
-// }
-// greet();
+function greet() {
+    console.log("Hii there ");
+    console.log("Hello pw Skills Learner ");
+}
+greet();
 
 
-// function sqx(x) {
+function sqx(x) {
 
-//     console.log(x * x);
-// }
-// sqx(5);
+    console.log(x * x);
+}
+sqx(5);
 
-// function main(a) {
-//     return a + a;
+function main(a) {
+    return a + a;
 
-// }
-// let result = main(10); //  a => Argument
+}
+let result = main(10); //  a => Argument
 
-// console.log(result);
+console.log(result);
 
-// Logic Build Base on Function 
+Logic Build Base on Function 
 
-// function user(a){
-//     if(a == 10){
-//         console.log("user is Valided");
-//     }
-//     else{
-//         console.log("User Unkonw");
-//     }
-// }
-// user(100);
+function user(a){
+    if(a == 10){
+        console.log("user is Valided");
+    }
+    else{
+        console.log("User Unkonw");
+    }
+}
+user(100);
 
-// // Addintion Fuction 
+// Addintion Fuction 
 
-// function AdditiOn(m,n){
-//     return m * n;
-// }
-// let result1 = AdditiOn(10,5);
-// console.log(result1);
+function AdditiOn(m,n){
+    return m * n;
+}
+let result1 = AdditiOn(10,5);
+console.log(result1);
 
 
 function syMessage() {
     console.log("Hii Pw Skills Learner & How are You Brother");
 
 }
-// syMessage();
-// syMessage();
-// syMessage(); 
-// syMessage();
-// syMessage(); 
+syMessage();
+syMessage();
+syMessage(); 
+syMessage();
+syMessage(); 
 
 function passMessage() {
     return "Gnanamani College of Technology Student ThanK You For Your Time "
