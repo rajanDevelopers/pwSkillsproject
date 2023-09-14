ec3e9dc785a2f5da923fbdfb3ec1e5c1937f89c2
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2023-09-14 06:32:54 +0530

    Update 6_Object_method.js

--- 6_Object_method.js
+++ 6_Object_method.js
@@ -11,7 +11,7 @@ function NewObject(i,n,s,c) {
 }
 
 let NewObjectValue =new NewObject(102,'Sanket Shing',12000,'patna');
-// console.table(NewObjectValue);
+ console.table(NewObjectValue);
 
 
 //Object Declaration 
@@ -22,50 +22,50 @@ let Studnt = {
     city: 'patna',
     salary: 25000
 }
-// console.log(Studnt);
+ console.log(Studnt);
 
 
 // Fetch the Object keys 
 
  let keys=Object.keys(Studnt);
- // console.log(keys);
+  console.log(keys);
 
 
  // fetch the value in Object 
 
   let value = Object.values(Studnt);
-    // console.table(value);
+     console.table(value);
 
   // when Access the Keys and Value Aslo in Object 
 
   let Entries = Object.entries(Studnt);
-  // console.log(Entries);
+   console.log(Entries);
 
   // update the value in Object 
   Studnt['city'] = 'Chennai'; 
- // console.log(Studnt);
+ console.log(Studnt);
 
   // Adding the extra value in Object 
   Studnt.age = 25;
- //  console.log(Studnt);
+  console.log(Studnt);
 
   // delete the method in Object 
 
   delete Studnt.age;
-  // console.log(Studnt);
+   console.log(Studnt);
 
   // Value ko nhi change karne denta hai to Object me method 
   // Not include the Extra value in Object Under 
   Object.freeze(Studnt);
 Studnt.age = 35;
-  // console.log(Studnt);
+ console.log(Studnt);
   
   // include the value extra in 
 
   // Update Allow ke liye in Object not include in New Value 
   Object.seal(Studnt);
   Studnt.salary = 105;
-  // console.log(Studnt);
+ console.log(Studnt);
 
   // when Object me new Object assign karna ho to 
 
